ce38ebf681f65336676f5256544f948a323d9706
Guilherme Ramos <guimeloramos@hotmail.com>
2018-11-20 23:11:09 -0200

    Adiciona funcionalidade de abrertura e leitura de arquivo

--- Downloader.Robot/Downloader.Robot/Program.cs
+++ Downloader.Robot/Downloader.Robot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace Downloader.Robot
     {
         static void Main(string[] args)
         {
+            if (!File.Exists(@"Credenciais.txt"))
+            {
+                throw new Exception("Deu ruim");
+            }
+
+            var credenciais = File.ReadAllLines(@"Credenciais.txt").ToArray();
+
+            if (credenciais.Length == 2)
+            {
+                var user = credenciais[0];
+                var password = credenciais[1];
+            }
+            else
+            {
+                throw new Exception("Arquivo com conteúdo não válido");
+            }
         }
     }
 }
