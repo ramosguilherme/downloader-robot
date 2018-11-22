1980bf27a7be590639b60f9f59ce275e1524b47b
Guilherme Ramos <guimeloramos@hotmail.com>
2018-11-21 23:28:04 -0200

    Tentativa de login

--- Downloader.Robot/Downloader.Robot/Program.cs
+++ Downloader.Robot/Downloader.Robot/Program.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Downloader.Robot
 {
@@ -45,17 +46,64 @@ namespace Downloader.Robot
                 var htmlDoc = client.Load(address);
                 var nodeUser = htmlDoc.DocumentNode.SelectSingleNode(@"//input[@id='email']");
                 var nodePassword = htmlDoc.DocumentNode.SelectSingleNode(@"//input[@id='pass']");
+                var nodeLogin = htmlDoc.DocumentNode.SelectSingleNode(@"//input[@name='Entrar']");
 
-                nodeUser.Attributes.Append("value");
-                nodeUser.SetAttributeValue("value", User);
+                nodeUser.Attributes.Add("value", User);
                 Log(nodeUser.OuterHtml);
 
-                nodePassword.Attributes.Append("value");
-                nodePassword.SetAttributeValue("value", Password);
+                nodePassword.Attributes.Add("value", Password);
                 Log(nodePassword.OuterHtml);
             }
 
+            StringBuilder sb = new StringBuilder();
+            Program.AppendParameter(sb, "email", User);
+            Program.AppendParameter(sb, "pass", Password);
+            byte[] byteArray = Encoding.UTF8.GetBytes(sb.ToString());
+
+            string url = "http://www.facebook.com/";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.AllowAutoRedirect = true;
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            var html = new HtmlDocument();
+            var res = client.Load(response.ResponseUri.ToString());
+
             Thread.Sleep(20000);
+        }
+
+        protected static void AppendParameter(StringBuilder sb, string name, string value)
+        {
+            string encodedValue = HttpUtility.UrlEncode(value);
+            sb.AppendFormat("{0}={1}&", name, encodedValue);
+        }
+
+        private void SendDataToService()
+        {
+            StringBuilder sb = new StringBuilder();
+            AppendParameter(sb, "value", User);
+            AppendParameter(sb, "value", Password);
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(sb.ToString());
+
+            string url = "http://www.facebook.com/";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
         }
 
         private static void Log(string message)
